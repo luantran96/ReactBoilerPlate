912f43395c349ea94ae59d30ef204db9dc48e200
Luan Tran <luan.phuoc.tran.96@gmail.com>
2019-01-30 20:57:47 -0800

    add animations to about info

--- src/About.jsx
+++ src/About.jsx
@@ -1,13 +1,38 @@
 import React from 'react';
+import { Motion, spring } from 'react-motion';
 
 class About extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      renderText: false,
+    };
+
+    this.handleScroll = this.handleScroll.bind(this);
   }
 
+  handleScroll() {
+    console.log(window.pageYOffset);
+    let y = window.pageYOffset;
 
+    if(y >= 4) {
+      this.setState({ renderText: true });
+    }
+
+    if(y === 0) {
+      this.setState({ renderText: false });
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('scroll', this.handleScroll);
+  }
 
   render() {
+
+    let { renderText } = this.state;
+
     const SVG = (
       <svg version="1.1" id="Calque_2" xmlns="http://www.w3.org/2000/svg" x="0px" y="0px" viewBox="0 0 635.3 844.6" enableBackground="new 0 0 635.3 844.6">
       <path id="z0" fill="none" stroke="#010202" strokeWidth="20" strokeLinejoin="round" strokeMiterlimit="10" d="M259.3,834.6
@@ -20,28 +45,34 @@ class About extends React.Component {
     );
 
     return (
-    <div id="about">
+    <Motion
+    defaultStyle={{opacity: 0}}
+    style={{opacity: renderText ? spring(1, {stiffness: 10, damping: 6}) : spring(0, {stiffness: 10, damping: 6})}}
+    >
+      {style => 
+        <div id="about" style={{opacity: style.opacity}}>
+          <div id="about-content1">
+            {SVG}
+            <div id="about-text">
+              <span className="text-light">FULL-STACK</span> <span className="text-bold">DEV</span>
+                <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                </p>
 
-      <div id="about-content1">
-        {SVG}
-        <div id="about-text">
-          <span className="text-light">FULL-STACK</span> <span className="text-bold">DEV</span>
-            <p>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
-
+              </div>
           </div>
-      </div>
-      <div id="about-content2">
-        <div id="about-text">
-           <span className="text-bold">Projects</span>
-            <p>
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            </p>
+          <div id="about-content2">
+            <div id="about-text">
+              <span className="text-bold">Projects</span>
+                <p>
+                "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
+                </p>
 
+              </div>
           </div>
-      </div>
-    </div>
+        </div>
+      }
+    </Motion>
     );
   }
 }
