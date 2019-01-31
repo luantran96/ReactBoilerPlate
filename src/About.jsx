4d2d3478e663e536bc70f7daeb449a7016e29348
Luan Tran <luan.phuoc.tran.96@gmail.com>
2019-01-30 19:33:24 -0800

    added animation to title page

--- src/About.jsx
+++ src/About.jsx
@@ -30,7 +30,6 @@ class About extends React.Component {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             </p>
 
-            <hr/>
           </div>
       </div>
       <div id="about-content2">
@@ -40,7 +39,6 @@ class About extends React.Component {
             "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
             </p>
 
-            <hr/>
           </div>
       </div>
     </div>
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,29 +1,40 @@
 import React from 'react';
-import { Motion, spring } from 'react-motion';
+import { StaggeredMotion, Motion, spring } from 'react-motion';
 import About from './About.jsx';
 
 class Home extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      scrollUp: false,
+    };
   }
+  // {opacity: spring(1, {stiffness: 4, damping: 1}), 
+  // y: spring(0, {stiffness: 140, damping: 30})
 
   render() {
+    let { scrollUp } = this.state;
     return (
       <div id="home">
-        <Motion
-        defaultStyle={{opacity: 0}} // starting style
-        style={{opacity: spring(1, {stiffness: 4, damping: 1} )}}  // Animate to
+        <StaggeredMotion
+        defaultStyles={[ {opacity: 0, y: 0}, {opacity: 0} ]} // starting style
+        styles={prevInterpolatedStyles => prevInterpolatedStyles.map((_, i) => {
+          return i === 0
+            ? {opacity: spring(1, {stiffness: 14, damping: 6})}
+            : {opacity: spring(prevInterpolatedStyles[i - 1].opacity, {stiffness: 14, damping: 6})}
+        })
+        }  
         >
-
-        {(style) => (
-          <div
-          id="header"
-          style={{
-            opacity: style.opacity,
-          }} 
-          >welcome</div>
+        {interpolatingStyles => (
+          <div id="header">
+            {interpolatingStyles.map((style, i) => {
+              return i === 0 ?
+              <div key={i} style={{opacity: style.opacity}}>Welcome</div> : <div key={i} style={{opacity: style.opacity}}>My name is Luan</div>
+            })}
+          </div>
         )}
-        </Motion>
+        </StaggeredMotion>
         <div className="down-button"> </div>
       </div>
     );
